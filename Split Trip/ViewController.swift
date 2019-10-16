4f402968e2331c0a0aebb8f22a84fad3ec6707bd
MasterYourMoney <seschwan@yahoo.com>
2019-10-15 20:34:59 -0600

    First Commit

---
+++ Split Trip/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Split Trip
+//
+//  Created by Seschwan on 10/15/19.
+//  Copyright © 2019 Seschwan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
