61e2ccaa664d9a54717249a7f449c6284ca91401
Eoin Lavery <dev@tenantc.co.uk>
2019-10-19 20:34:15 +0100

    Added and configured AddTripVC with ParticipantCollectionView and TripSettingsTableView

--- Split Trip/View Controllers/HomeScreen/HomeTabBarMenu/TripTableViewCell.swift
+++ Split Trip/View Controllers/HomeScreen/HomeTabBarMenu/TripTableViewCell.swift
@@ -17,19 +17,14 @@ class TripTableViewCell: UITableViewCell {
     
     let cellBackgroundGrayColor = UIColor(red:0.95, green:0.95, blue:0.95, alpha:1.0)
     let cellTextColor = UIColor(red:0.22, green:0.08, blue:0.36, alpha:1.0)
+    let cellValueColor = UIColor(red:0.42, green:0.15, blue:1.00, alpha:1.0)
     
     override func awakeFromNib() {
         super.awakeFromNib()
         bgView.backgroundColor = cellBackgroundGrayColor
         bgView.layer.cornerRadius = 8
         tripTitleLabel.textColor = cellTextColor
-        tripOwedLabel.textColor = cellTextColor
+        tripOwedLabel.textColor = cellValueColor
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-    }
-    
-    
     
 }
