c079f4effbb5aa2c5dc12830e436df8adc4e2825
MasterYourMoney <seschwan@yahoo.com>
2019-10-23 19:40:07 -0600

    Signup networking complete

--- Split Trip/View Controllers/SignUpVC.swift
+++ Split Trip/View Controllers/SignUpVC.swift
@@ -10,9 +10,11 @@ import UIKit
 
 class SignUpVC: UIViewController {
     
+    let createUserController = CreateUserController()
+    
     // MARK: - Outlets
     @IBOutlet weak var nameTextField: UITextField!
-    @IBOutlet weak var phoneTextField: UITextField!
+    @IBOutlet weak var usernameTextField: UITextField!
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     
@@ -27,6 +29,19 @@ class SignUpVC: UIViewController {
     
     @IBAction func signUpBtnPressed(_ sender: LoginSignupCustomButton) {
         // Need to create a new user here and check for the values in the text boxes.
+        guard let name = nameTextField.text,
+            let username = usernameTextField.text,
+            let email = emailTextField.text,
+            let password = passwordTextField.text else { return }
+        
+        let user = createUserController.createUser(name: name, username: username, password: password, email: email)
+        
+        createUserController.signUpNewUser(user: user, completion: { (error) in
+            if let error = error {
+                NSLog("Error creating a new user: \(error)")
+                return
+            }
+        })
         
     }
     
